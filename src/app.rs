1f253467c45bf73fbef65a98878522aab49bfff8
hamesjan <jhan31903@gmail.com>
2025-11-23 13:10:40 -0800

    added tabview

--- src/app.rs
+++ src/app.rs
@@ -7,7 +7,7 @@ use tracing::{debug, info};
 
 use crate::{
     action::Action,
-    components::{Component, fps::FpsCounter, home::Home},
+    components::{Component, fps::FpsCounter, home::Home, navbar::NavBar},
     config::Config,
     tui::{Event, Tui},
 };
@@ -37,7 +37,7 @@ impl App {
         Ok(Self {
             tick_rate,
             frame_rate,
-            components: vec![Box::new(Home::new()), Box::new(FpsCounter::default())],
+            components: vec![Box::new(Home::new()), Box::new(FpsCounter::default()), Box::new(NavBar::new())],
             should_quit: false,
             should_suspend: false,
             config: Config::new()?,
